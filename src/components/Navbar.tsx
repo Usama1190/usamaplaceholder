a992cc044356997b1a80b3156975a05d6adc58ec
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-12-02 00:19:33 +0500

    Add linking names on Dec 02, 24

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,10 @@ const Navbar = () => {
         <a href="#"><strong>USAMAPlaceholder</strong></a>
       </div>
       <div className='w-[45%] flex justify-evenly'>
-        <a href="#">Guide</a>
-        <a href="#">Sponsor this project</a>
-        <a href="#">Blog</a>
-        <a href="#">My USAMA Server</a>
+        <a href="#Guide">Guide</a>
+        <a href="#Sponsor-this-project">Sponsor this project</a>
+        <a href="#Blog">Blog</a>
+        <a href="#My-USAMA-Server">My USAMA Server</a>
       </div>
     </div>
   )
